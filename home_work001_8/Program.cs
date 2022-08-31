a6d4341385de6f0165659701afa78d3de59835e8
Prizrak_Leonis <prizrak2leonis@gmail.com>
2022-08-31 21:27:34 +1000

    add task 8

---
+++ home_work001_8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Хотите я назову все чётные числа от 1 до вашего числа?");
+Console.Write("Введите число: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+while (numN < 2)
+{
+    Console.Write("Введите число больше 1: ");
+    numN = Convert.ToInt32(Console.ReadLine());
+}    
+int n = 2;
+while (n <= numN )
+{
+if (n%2 == 0)
+    Console.Write(n + " ");
+n++;
+}
