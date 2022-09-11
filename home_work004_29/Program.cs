b6c1717834811f67bc7b37313b8551d11508bd35
Prizrak_Leonis <prizrak2leonis@gmail.com>
2022-09-11 19:58:39 +1000

    add task 29

---
+++ home_work004_29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    // array[i] = new Random().Next(0, 101);
+    Console.Write("Введите " + (i + 1) + "-е число массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
