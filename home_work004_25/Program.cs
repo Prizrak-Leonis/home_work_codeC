e60a18373175986269ee922c5c7a21717a9472ae
Prizrak_Leonis <prizrak2leonis@gmail.com>
2022-09-11 19:14:19 +1000

    add task 25

---
+++ home_work004_25/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Возведение числа в натуральную степень.");
+Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите в какую степень нужно возвести введёное выше число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numA + "^" + numB + " = " + Math.Pow(numA, numB));
