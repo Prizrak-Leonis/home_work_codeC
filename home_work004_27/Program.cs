2fb805d0c2435abe277b36c90c69c7f15c781bd7
Prizrak_Leonis <prizrak2leonis@gmail.com>
2022-09-11 19:48:32 +1000

    add task 27

---
+++ home_work004_27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Хотите я посчитаю сумму всех цифр в названном Вами числе?");
+Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (numA > 0)
+{
+    sum += numA % 10;
+    numA /= 10;
+}
+Console.WriteLine("Сумма цифр вашего числа равна: " + sum);
