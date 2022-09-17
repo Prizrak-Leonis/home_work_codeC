da02634e8da9d7cd3e4d9fabe6ce906d89062720
Prizrak_Leonis <prizrak2leonis@gmail.com>
2022-09-17 17:38:30 +1000

    add task 41

---
+++ home_work006_41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите кол-во вводимых чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+    Console.Write($"Введите {i + 1}-е числo: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    if (N > 0)
+        count++;
+}
+Console.WriteLine("Чисел больше 0 введено: " + count);
+
