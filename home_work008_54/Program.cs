19032f37c2fc1fac4aad4da1884eab0c3e5701c1
Prizrak_Leonis <prizrak2leonis@gmail.com>
2022-09-24 17:33:15 +1000

    add task 54, 56

---
+++ home_work008_54/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов. Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void WriteMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < 10)
+                Console.Write(" ");
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int lineMinimalSumm(int[,] matrix)
+{
+    int summMin = 0;
+    int lineMin = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int summ = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            summ += matrix[i, j];
+        }
+        if (i == 0)
+            summMin = summ;
+        else if (summ < summMin)
+        {
+            summMin = summ;
+            lineMin = i;
+        }
+    }
+    return lineMin;
+}
+
+Console.Write("Введите кол-во строк в матрице: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов в матрице: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+int[,] massive = new int[lines, colums];
+InputMatrix(massive);
+WriteMatrix(massive);
+int lineMinSumm = lineMinimalSumm(massive);
+Console.WriteLine();
+Console.Write("Строка с минимальной суммой всех значений: ");
+for (int i = 0; i < massive.GetLength(1) - 1; i++)
+{
+    Console.Write(massive[lineMinSumm, i] + ", ");
+}
+Console.Write(massive[lineMinSumm, massive.GetLength(1) - 1]);
---
+++ home_work008_56/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(2, 5);
+        }
+    }
+}
+
+void WriteMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < 10)
+                Console.Write(" ");
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Перемножение матриц возможно только в случае когда кол-во стобцов первой матрицы совпадает с кол-во строк вo второй");
+Console.Write("Введите кол-во строк в вервой матрице: ");
+int lines1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов в первой матрице: ");
+int colums1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во строк вщ второй матрице: ");
+int lines2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов во второй матрице: ");
+int colums2 = Convert.ToInt32(Console.ReadLine());
+
+if (colums1 == lines2)
+{
+    int[,] massive1 = new int[lines1, colums1];
+    int[,] massive2 = new int[lines2, colums2];
+    int[,] massiveResult = new int[lines1, colums2];
+
+    InputMatrix(massive1);
+    WriteMatrix(massive1);
+
+    InputMatrix(massive2);
+    WriteMatrix(massive2);
+
+    for (int i = 0; i < massiveResult.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiveResult.GetLength(1); j++)
+        {
+            int temp = 0;
+            for (int k = 0; k < colums1; k++)
+            {
+                temp += massive1[i,k]*massive2[k,j];
+            }
+        massiveResult[i,j] = temp;
+        }
+    }
+    WriteMatrix(massiveResult);
+}
+else
+    Console.Write("Невозможно перемножить такие матрицы");
