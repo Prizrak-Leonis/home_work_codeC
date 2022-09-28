8c5d9506618ec7b204df79780d007b5df1a69261
Prizrak_Leonis <prizrak2leonis@gmail.com>
2022-09-28 19:03:28 +1000

    add home work 9

---
+++ home_work009_66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Сумма натуральных элементов в промежутке от M до N");
+Console.Write("Введите число М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int summNumbers(int n, int m)
+{
+    if (n == m)
+        return n;
+    return summNumbers(n + 1, m) + n;
+}
+
+if (N > M)
+    Console.WriteLine("Сумма всех элементов = " + summNumbers(M, N));
+else
+    Console.WriteLine("Сумма всех элементов = " + summNumbers(N, M));
---
+++ home_work009_68/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+double Akerman(double m, double n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Akerman(m - 1, 1);
+    return Akerman(m - 1, Akerman(m, n - 1)); // m > 0, n > 0
+}
+
+Console.WriteLine(Akerman(5, 3));
